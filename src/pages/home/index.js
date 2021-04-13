cfe8db22e157794de8b8daafcdbee4d21c105670
Emilio Villuendas Zambrana <emilio.villuendas@adevinta.com>
2021-04-13 18:00:37 +0200

    add logical domain

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,34 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import Carrusel from 'carrusel-package'
 import {Context} from '../../context'
 
 import {Aside} from '../../components/aside/index'
+import SliderData from '../../utils/SliderData'
 
 const baseClass = 'wb-Home'
 
 export function Home() {
-  const {i18n} = useContext(Context)
+  const {i18n, domain} = useContext(Context)
+  const [slides, setSlides] = useState(SliderData)
+  const [uploadedImages, setUploadedImages] = useState([])
+
+  const handleCurrentChange = current => {
+    if (uploadedImages.find(image => image === current)) return
+
+    setUploadedImages(preState => [...preState, current])
+
+    const isRequieredRequest =
+      current !== 0 && current % 3 === 0 && current < 10
+
+    if (isRequieredRequest)
+      domain
+        .get('retrieve_photo_list_use_case')
+        .execute({page: current})
+        .then(response => {
+          const {photos} = response
+          setSlides(preState => [...preState, ...photos])
+        })
+  }
 
   return (
     <div className={baseClass}>
@@ -22,7 +43,7 @@ export function Home() {
         <h1 className={`${baseClass}-title`}>{i18n.t('PAGE.HOME.TITLE')}</h1>
         <p>{i18n.t('PAGE.HOME.FIRST_POST_TEXT')}</p>
         <div className={`${baseClass}-carrusel`}>
-          <Carrusel />
+          <Carrusel onCurrentChange={handleCurrentChange} slides={slides} />
         </div>
         <p>{i18n.t('PAGE.HOME.SECOND_POST_TEXT')}</p>
         <p>{i18n.t('PAGE.HOME.SECOND_POST_TEXT')}</p>
---
+++ src/utils/SliderData.js
@@ -0,0 +1,24 @@
+const SliderData = [
+  {
+    image:
+      'https://moto.suzuki.es/assets/images/listado_motos/2021/1050XT/DL1050RCM1_YVB_Right.jpg',
+    id: 'b'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80',
+    id: 'a'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80',
+    id: 'c'
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80',
+    id: 'd'
+  }
+]
+
+export default SliderData
